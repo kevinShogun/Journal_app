817e10b04d0b0d564e50758fa457e47e85873a7c
Kevin Garcia <54457637+kevinShogun@users.noreply.github.com>
2021-04-20 17:18:00 -0600

    Inital Commit

---
+++ src/Journal-App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const JournalApp = () => {
+    return (
+        <div>
+            <h1>Journal_App</h1>
+        </div>
+    )
+}
---
+++ src/components/auth/RegisterScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const RegisterScreen = () => {
+    return (
+        <div>
+            <h1>
+                Register Screeen
+            </h1>
+        </div>
+    )
+}
